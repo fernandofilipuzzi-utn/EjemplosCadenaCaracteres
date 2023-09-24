b86fa89d115cbf8f1b97991301449ed0973841bf
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-09-23 21:05:43 -0300

    iniciando ejemplos

---
+++ EjemploExpresionesRegulares/Ej1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //encontrar todas las coincidencias y la posición.
+            string texto = "235645623523236422347";
+            
+            string patron = "23[0-9]";
+            
+            Match m=Regex.Match(texto, patron);
+
+            Console.WriteLine($"{"coincidencia",20}:{"posición",10}");
+            while (m.Success)
+            {
+                Console.WriteLine($"{m.ToString(),20}:{m.Index,10}");
+                m =m.NextMatch();
+            }
+            /*
+                 coincidencia:  posición
+                 235:         0
+                 235:         7
+                 232:        10
+                 234:        17
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjemploExpresionesRegulares/Ej2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //encontrar todas las subcadenas que contengan digitos no numericos 
+            //y la posición de cada coincidencia.
+            string texto = "2356ab45623FG523DC23C64223EFD47";
+
+            //letras[a-z,A-Z] que se repiten al menos una vez {1,}
+            string patron = "[a-z,A-Z]{1,}";
+            // se podría escribir tambien
+            //string patron = "([a-z,A-Z]{1,})";
+
+            Match m = Regex.Match(texto, patron);
+
+            Console.WriteLine($"{"coincidencia",20}:{"posición",10}");
+            while (m.Success)
+            {
+                Console.WriteLine($"{m.ToString(),20}:{m.Index,10}");
+                m = m.NextMatch();
+            }
+            /*
+        coincidencia:  posición
+                  ab:         4
+                  FG:        11
+                  DC:        16
+                   C:        20
+                 EFD:        26
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
